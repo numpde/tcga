9325ddc2002da7c0c3521cd978292e11b22d7064
RA <numpde@null.net>
2020-06-17 11:23:47 +0200

    Initial

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", 'r') as fd:
+    long_description = fd.read()
+
+setuptools.setup(
+    name="tcga",
+    version="0.0.0",
+    author="RA",
+    author_email="numpde@null.net",
+    keywords="computational biology bioinformatics genetics",
+    description="Computational biology & bioinformatics utils",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/numpde/tcga",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[''],
+
+    test_suite="nose.collector",
+    tests_require=["nose"],
+)
---
+++ tcga/gencode/__init__.py
@@ -0,0 +1,1 @@
+from .gencode import genetic_code
