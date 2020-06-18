b6b350a8267dc79f53a117c2c4314958c46c615b
RA <numpde@null.net>
2020-06-18 14:14:51 +0200

    Hide Join class

--- tcga/utils/utils.py
+++ tcga/utils/utils.py
@@ -4,21 +4,19 @@ import typing
 import itertools
 
 
-class _Join:
-    def __init__(self, as_type=str):
-        self._result_type = as_type
+class _:
+    class Join:
+        def __init__(self, as_type=str):
+            self._result_type = as_type
 
-    def __getitem__(self, as_type=str):
-        return _Join(as_type)
+        def __getitem__(self, as_type=str):
+            return _.Join(as_type)
 
-    def __call__(self, iterable: typing.Iterable):
-        if self._result_type is str:
-            return ''.join(itertools.chain.from_iterable(iterable))
-        else:
-            return self._result_type(itertools.chain.from_iterable(iterable))
-
-
-join = _Join()
+        def __call__(self, iterable: typing.Iterable):
+            if self._result_type is str:
+                return ''.join(itertools.chain.from_iterable(iterable))
+            else:
+                return self._result_type(itertools.chain.from_iterable(iterable))
 
 
 class First:
@@ -51,7 +49,7 @@ class First:
         return self
 
     def join(self, as_type):
-        self.__ff.append(Join(as_type))
+        self.__ff.append(_.Join(as_type))
         return self
 
     def __matmul__(self, other):
@@ -64,3 +62,6 @@ class First:
 
     def __getitem__(self, x):
         return self(x)
+
+
+join = _.Join()
