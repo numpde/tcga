43a0b799e00d7d10c9f88f88514cfd6c70855ab2
RA <numpde@null.net>
2020-06-18 18:16:34 +0200

    Version checkpoint

--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 import setuptools
 
-with open("README.md", 'r') as fd:
-    long_description = fd.read()
+# with open("README.md", 'r') as fd:
+#     long_description = fd.read()
 
 setuptools.setup(
     name="tcga",
-    version="0.0.6",
+    version="0.0.7",
     author="RA",
     author_email="numpde@null.net",
     keywords="computational biology bioinformatics genetics",
     description="Computational biology & bioinformatics utils",
-    long_description=long_description,
+    long_description="Please see the [github page](https://github.com/numpde/tcga).",
     long_description_content_type="text/markdown",
     url="https://github.com/numpde/tcga",
     packages=setuptools.find_packages(),
