1849d2de68b839c7cd9004d9c58e03722af8bb90
RA <numpde@null.net>
2020-06-25 02:42:35 +0200

    Overhaul

--- tcga/download/download.py
+++ tcga/download/download.py
@@ -2,6 +2,7 @@
 
 
 import io
+import pandas
 import base64
 import json
 import contextlib
@@ -9,87 +10,134 @@ import pathlib
 import zipfile
 import typing
 import datetime
+import inspect
 
 import shutil
 import urllib.request
 
 
-class Download:
-    default_folder = pathlib.Path(pathlib.Path.cwd() / "download_cache")
+class _:
+    # class __Download__(type):
+    #     def __init__(cls, *args, **kwargs):
+    #         cls._last_used_folder = None
+    #         super().__init__(*args, **kwargs)
+    #
+    #     @property
+    #     def last_used_folder(cls):
+    #         return cls._last_used_folder
+    #
+    #     @last_used_folder.setter
+    #     def last_used_folder(cls, value):
+    #         cls._last_used_folder = value
 
-    def __init__(self, url: str, zipped=True):
-        if not zip:
-            raise NotImplementedError("Only zipped=True is implemented.")
-        self.meta = {'source': url}
+    class Download:
+        def __init__(self, **kwargs):
+            self._ = pandas.Series({
+                'meta': {}, 'url': None, 'local_folder': None, 'renew': False,
+                **(kwargs or {}),
+            })
 
-    def with_meta(self, meta: dict):
-        assert 'source' not in meta
-        assert 'datetime' not in meta
-        assert json.loads(json.dumps(meta)) == meta, "The specified metadata is not json-stable."
-        self.meta = {**self.meta, **meta}
-        return self
+        def to(self, *, rel_path=None, abs_path=None):
+            if abs_path:
+                assert not rel_path
+                local_folder = pathlib.Path(abs_path)
+            else:
+                assert not abs_path
+                f = inspect.currentframe().f_back
+                local_folder = (pathlib.Path(f.f_code.co_filename).parent / rel_path)
+            self._.local_folder = local_folder
+            return self
 
-    def to(self, local_folder):
-        url = self.meta['source']
-        assert url
-        name = base64.urlsafe_b64encode(url.encode()).decode()
-        local_file = (pathlib.Path(local_folder) / name).with_suffix(".zip")
+        def __call__(self, url: str):
+            return type(self)(**{**self._, 'url': url})
 
-        if not local_file.exists():
-            local_file.parent.mkdir(exist_ok=True, parents=True)
+        def renew(self, renew=False):
+            return type(self)(**{**self._, 'renew': renew})
 
-            with contextlib.closing(urllib.request.urlopen(url=url)) as rd:
-                with zipfile.ZipFile(local_file, mode='w') as zf:
-                    with zf.open("data", mode='w') as fd:
-                        shutil.copyfileobj(rd, fd)
-                    with zf.open("meta", mode='w') as fd:
-                        assert 'datetime' not in self.meta
-                        self.meta['datetime'] = datetime.datetime.now(tz=datetime.timezone.utc).isoformat(sep=' ')
-                        fd.write(json.dumps(self.meta).encode())
+        @property
+        def local_folder(self) -> pathlib.Path:
+            return self._.local_folder
 
-        assert local_file.exists()
+        def with_meta(self, meta: dict):
+            assert 'source' not in meta
+            assert 'datetime' not in meta
+            assert json.loads(json.dumps(meta)) == meta, "The specified metadata should be json-stable."
+            return type(self)(**{**self._, 'meta': meta})
 
-        with zipfile.ZipFile(local_file, mode='r') as zf:
-            with zf.open("meta") as fd:
-                meta = json.loads(fd.read().decode())
-                for (k, v) in self.meta.items():
-                    if k in meta:
-                        assert meta[k] == v, "The specified metadata differs from what is in the file."
-                meta = {**self.meta, **meta}
+        @property
+        def now(self):
+            if not self._.url:
+                raise RuntimeError("What do you want to download?")
+            if not self._.local_folder:
+                raise RuntimeError("Use .to(...) to set the local folder before downloading.")
 
-        class Local:
-            def __init__(self, local_file: pathlib.Path, meta: dict):
-                self.local_file = local_file
-                self.meta = meta
+            name = base64.urlsafe_b64encode(self._.url.encode()).decode()
+            local_file = (pathlib.Path(self._.local_folder) / name).with_suffix(".zip")
 
-            @contextlib.contextmanager
-            def open(self, mode='rb'):
-                assert mode in ["r", "rb"]
-                with zipfile.ZipFile(self.local_file, mode='r') as _:
-                    with _.open("data") as _:
-                        if mode == "rb":
-                            yield _
-                        else:
-                            yield io.TextIOWrapper(_)
+            if self._.renew or not local_file.exists():
+                local_file.parent.mkdir(exist_ok=True, parents=True)
 
-            @property
-            def bytes(self) -> bytes:
-                with self.open(mode='rb') as _:
-                    return _.read()
+                with contextlib.closing(urllib.request.urlopen(url=self._.url)) as rd:
+                    with zipfile.ZipFile(local_file, mode='w') as zf:
+                        with zf.open("data", mode='w') as fd:
+                            shutil.copyfileobj(rd, fd)
+                        with zf.open("meta", mode='w') as fd:
+                            assert 'url' not in self._.meta
+                            assert 'datetime' not in self._.meta
+                            self._.meta['source'] = self._.url
+                            self._.meta['datetime'] = datetime.datetime.now(tz=datetime.timezone.utc).isoformat(sep=' ')
+                            fd.write(json.dumps(self._.meta).encode())
 
-            @property
-            def text(self) -> str:
-                with self.open(mode='r') as _:
-                    return _.read()
+            assert local_file.exists()
 
-        return Local(local_file, meta)
+            with zipfile.ZipFile(local_file, mode='r') as zf:
+                with zf.open("meta") as fd:
+                    meta = json.loads(fd.read().decode())
+                    for (k, v) in self._.meta.items():
+                        if k not in meta:
+                            raise RuntimeWarning("The specified metadata is not in the file. Use renew(True).")
+                        if not (meta[k] == v):
+                            raise RuntimeWarning(
+                                "The specified metadata differs from the file. Use renew(True).")
+                    meta = {**self._.meta, **meta}
 
+            class Local:
+                def __init__(self, local_file: pathlib.Path, meta: dict):
+                    self.local_file = local_file
+                    self.meta = meta
+
+                def __call__(self):
+                    return self
+
+                @contextlib.contextmanager
+                def open(self, mode='rb'):
+                    assert mode in ["r", "rb"]
+                    with zipfile.ZipFile(self.local_file, mode='r') as _:
+                        with _.open("data") as _:
+                            if mode == "rb":
+                                yield _
+                            else:
+                                yield io.TextIOWrapper(_)
+
+                @property
+                def bytes(self) -> bytes:
+                    with self.open(mode='rb') as _:
+                        return _.read()
+
+                @property
+                def text(self) -> str:
+                    with self.open(mode='r') as _:
+                        return _.read()
+
+            return Local(local_file, meta)
+
+
+download = _.Download()
 
 if __name__ == '__main__':
     def example1():
         url = "https://www.ebi.ac.uk/ena/browser/api/fasta/J02459.1"
-        x = Download(url).to(Download.default_folder)
-
+        x = download(url).now
         print("[Meta]")
         print(x.meta)
 
@@ -102,10 +150,15 @@ if __name__ == '__main__':
 
     def example2():
         url = "ftp://ftp.genome.jp/pub/db/community/aaindex/aaindex.doc"
-        x = Download(url).with_meta({'xyz': 3}).to(Download.default_folder)
-        print(x.meta)
-        print(x.text[0:100], "...")
+        # download(url).now
+        download.to(rel_path="download_cache")
+        x = download(url).with_meta({'remember': "Nov 5th"}).now
+        # x = download(url).with_meta({'remember': "Dec 5th"}).renew(True).now
+        print("Last used:", download.local_folder)
+        y = download(url).now
+        print(y.meta)
 
+    # from tcga import rootpath
 
     example2()
     example1()
