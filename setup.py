bd2c1e91143b4f3a94ffe15b40a5640c5ffc0f58
RA <numpde@null.net>
2020-06-25 09:44:31 +0200

    Version checkpoint

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import setuptools
 
 setuptools.setup(
     name="tcga",
-    version="0.0.11",
+    version="0.0.12",
     author="RA",
     author_email="numpde@null.net",
     keywords="computational biology bioinformatics genetics",
